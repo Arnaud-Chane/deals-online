c5dfda87459aae8627bca57d91c3003a7d72c3a2
Arnaud-Chane <arnaud.chane.dev@gmail.com>
2023-05-07 15:07:49 +0200

    Making it responsive

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -24,55 +24,57 @@ function Home() {
             alt="pic of meme"
           />
         </div>
-        <div className="main-img-title">
-          <h2>Nulla officia aute do velit occaecat id do.</h2>
-        </div>
-        <div className="main-img-note">
-          <small>
-            Mollit consequat cillum pariatur esse in in non mollit excepteur
-            labore.
-          </small>
-        </div>
-        <div className="main-para-ctn">
-          <div className="main-para1">
-            Anim irure magna fugiat sint sint tempor id eu tempor.Labore ex
-            aliqua cupidatat elit cillum.Cupidatat dolore tempor eiusmod ut
-            proident deserunt fugiat. Tempor aliqua dolore cillum ipsum aute
-            adipisicing est ullamco veniam ipsum. Aliqua quis dolore labore
-            exercitation enim in excepteur quis dolore proident.
+        <div className="content">
+          <div className="main-img-title">
+            <h2>Nulla officia aute do velit occaecat id do.</h2>
           </div>
-          <div className="main-para2">
-            Anim irure magna fugiat sint sint tempor id eu tempor.Labore ex
-            aliqua cupidatat elit cillum.Cupidatat dolore tempor eiusmod ut
-            proident deserunt fugiat. Tempor aliqua dolore cillum ipsum aute
-            adipisicing est ullamco veniam ipsum. Aliqua quis dolore labore
-            exercitation enim in excepteur quis dolore proident.
+          <div className="main-img-note">
+            <small>
+              Mollit consequat cillum pariatur esse in in non mollit excepteur
+              labore.
+            </small>
           </div>
-          <div className="main-para3">
-            Anim irure magna fugiat sint sint tempor id eu tempor.Labore ex
-            aliqua cupidatat elit cillum.Cupidatat dolore tempor eiusmod ut
-            proident deserunt fugiat. Tempor aliqua dolore cillum ipsum aute
-            adipisicing est ullamco veniam ipsum. Aliqua quis dolore labore
-            exercitation enim in excepteur quis dolore proident.
-          </div>
-          <div className="btn-container">
-            <div className="btn btn-amazon">
-              <a href="#">
-                <span>listen on Audible{" "}</span>
-                <img src="https://picsum.photos/20" alt="pic of meme" />{" "}
-              </a>
+          <div className="main-para-ctn">
+            <div className="main-para1">
+              Anim irure magna fugiat sint sint tempor id eu tempor.Labore ex
+              aliqua cupidatat elit cillum.Cupidatat dolore tempor eiusmod ut
+              proident deserunt fugiat. Tempor aliqua dolore cillum ipsum aute
+              adipisicing est ullamco veniam ipsum. Aliqua quis dolore labore
+              exercitation enim in excepteur quis dolore proident.
             </div>
-            <div className="btn btn-kindle">
-              <a href="#">
-                <span>listen on Audible{" "}</span>
-                <img src="https://picsum.photos/20" alt="pic of meme" />{" "}
-              </a>
+            <div className="main-para2">
+              Anim irure magna fugiat sint sint tempor id eu tempor.Labore ex
+              aliqua cupidatat elit cillum.Cupidatat dolore tempor eiusmod ut
+              proident deserunt fugiat. Tempor aliqua dolore cillum ipsum aute
+              adipisicing est ullamco veniam ipsum. Aliqua quis dolore labore
+              exercitation enim in excepteur quis dolore proident.
             </div>
-            <div className="btn btn-audible">
-              <a href="#">
-                <span>listen on Audible{" "}</span>
-                <img src="https://picsum.photos/20" alt="pic of meme" />{" "}
-              </a>
+            <div className="main-para3">
+              Anim irure magna fugiat sint sint tempor id eu tempor.Labore ex
+              aliqua cupidatat elit cillum.Cupidatat dolore tempor eiusmod ut
+              proident deserunt fugiat. Tempor aliqua dolore cillum ipsum aute
+              adipisicing est ullamco veniam ipsum. Aliqua quis dolore labore
+              exercitation enim in excepteur quis dolore proident.
+            </div>
+            <div className="btn-container">
+              <div className="btn btn-a btn-amazon">
+                <a href="#">
+                  <span>How to talk to your cat on Amazon </span>
+                  <img src="https://picsum.photos/20" alt="pic of meme" />{" "}
+                </a>
+              </div>
+              <div className="btn btn-a btn-kindle">
+                <a href="#">
+                  <span>How to talk to your cat on Kindle</span>
+                  <img src="https://picsum.photos/20" alt="pic of meme" />{" "}
+                </a>
+              </div>
+              <div className="btn btn-audible">
+                <a href="#">
+                  <span>listen on Audible </span>
+                  <img src="https://picsum.photos/20" alt="pic of meme" />{" "}
+                </a>
+              </div>
             </div>
           </div>
         </div>
